4a429b7296ab4851e2fd4a03a48a7fd0d341c548
nataliiasirenko <sirenko.nataliia@hotmail.com>
2023-11-06 18:28:59 +0000

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -42,21 +42,3 @@ const promise = new Promise((resolve, reject) => {
     }, 2000);
   });
   
-  promise
-    .then(value => {
-      console.log(value); // 5
-      return value * 2;
-    })
-    .then(value => {
-      console.log(value); // 10
-      return value * 3;
-    })
-    .then(value => {
-      console.log(value); // 30
-    })
-    .catch(error => {
-      console.log(error);
-    })
-    .finally(() => {
-      console.log("Final task");
-    });
